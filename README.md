351e52c84d60f6bdd58b7ca69f1390e23024bb2f
aleferu <aleferu888@gmail.com>
2023-09-23 13:06:22 +0200

    feat: input from file

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Sudoku solver written in C
 
-Solver is done, want to make a CLI that uses csv.
+Sudoku solver using plain C and the backtracking algorithm.
+
+## Usage
+
+```console
+$ sudoku-solver input1.csv input2.csv input3.csv
+```
+
+Doesn't have to be a csv, the program will pick up the first 81 digits of each file.
+
+## Build
+
+```console
+$ ./build.sh
+```
+
+OR
+
+```console
+$ cc sudoku-solver.c -o sudoku-solver
+```
+
+cc, gcc, clang... Whatever
--- sudoku-solver.c
+++ sudoku-solver.c
@@ -9,6 +9,23 @@
 #define grid_type unsigned char
 
 
+// Just have 81 digits anywhere and it'll be fine
+bool read_from_csv_and_close(FILE* f, grid_type grid[GRID_SIZE]) {
+    size_t index = 0;
+    int ch;
+    while ((ch = fgetc(f)) != EOF && index < GRID_SIZE) {
+        int num = ch - 48;
+        if (num < 0 || num > 9) {
+            continue;
+        }
+        grid[index] = (unsigned char) num;
+        ++index;
+    }
+    fclose(f);
+    return index == GRID_SIZE;
+}
+
+
 void modify_grid_at(grid_type grid[GRID_SIZE], grid_type row, grid_type column, grid_type number) {
     grid[row * N + column] = number;
 }
@@ -105,22 +122,55 @@ bool grid_solve(grid_type grid[GRID_SIZE]) {
 }
 
 
-int main(void) {
-    grid_type grid[GRID_SIZE] = {
-        4, 0, 0, 0, 0, 0, 0, 0, 3,
-        0, 0, 8, 0, 7, 0, 4, 5, 0,
-        0, 9, 0, 0, 0, 6, 0, 0, 0,
-        0, 0, 0, 0, 3, 0, 0, 0, 8,
-        0, 0, 1, 0, 0, 0, 0, 0, 2,
-        6, 0, 0, 0, 0, 7, 1, 3, 0,
-        0, 0, 4, 0, 5, 0, 7, 1, 0,
-        8, 0, 0, 4, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 2, 0, 0
-    };
+int main(int argc, char const *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "ERROR: Please provide files.\n");
+        return 1;
+    }
 
-
-    printf("%d\n", grid_solve(grid));
-    grid_print(grid);
+    grid_type grid[GRID_SIZE];
+    FILE* f;
+    for (size_t i = 1; i < (size_t) argc; ++i) {
+        f = fopen(argv[i], "r");
+        if (!f) {
+            fprintf(stderr, "ERROR: Failed to open file \"%s\". Skipping...\n", argv[i]);
+            continue;
+        };
+        if (!read_from_csv_and_close(f, grid)) {
+            fprintf(stderr, "ERROR: Failed to read from file \"%s\". Skipping...\n", argv[i]);
+            continue;
+        }
+        printf("INFO: Original input of \"%s\":\n", argv[i]);
+        grid_print(grid);
+        if (!grid_solve(grid)) {
+            printf("WARNING: A solution was not found. Skipping...\n\n");
+            continue;
+        }
+        printf("INFO: Calculated solution of the sudoku:\n");
+        grid_print(grid);
+        printf("\n");
+    }
 
     return 0;
 }
+
+
+// int main(void) {
+//     grid_type grid[GRID_SIZE] = {
+//         4, 0, 0, 0, 0, 0, 0, 0, 3,
+//         0, 0, 8, 0, 7, 0, 4, 5, 0,
+//         0, 9, 0, 0, 0, 6, 0, 0, 0,
+//         0, 0, 0, 0, 3, 0, 0, 0, 8,
+//         0, 0, 1, 0, 0, 0, 0, 0, 2,
+//         6, 0, 0, 0, 0, 7, 1, 3, 0,
+//         0, 0, 4, 0, 5, 0, 7, 1, 0,
+//         8, 0, 0, 4, 0, 0, 0, 0, 0,
+//         0, 0, 0, 0, 0, 0, 2, 0, 0
+//     };
+
+
+//     printf("%d\n", grid_solve(grid));
+//     grid_print(grid);
+
+//     return 0;
+// }
